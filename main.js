786f4443eb5b71af28947db6aefbbc6b0d9df817
mariaboruck <maria.boruck@escola.pr.gov.br>
2026-06-11 11:30:30 +0000

    Aula 06

--- main.js
+++ main.js
@@ -14,8 +14,14 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo1 = new Date("2027-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2027-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2027-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2027-02-01T00:00:00");
+
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo) {
   let tempoAtual = new Date();
@@ -24,6 +30,12 @@ function calculaTempo(tempoObjetivo) {
   let minutos = Math.floor(segundos / 60);
   let horas = Math.floor(minutos / 60);
   let dias = Math.floor(horas / 24);
+
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24;
+
+  if (tempoFinal > 0) {
   return (
     dias +
     " dias " +
@@ -34,4 +46,20 @@ function calculaTempo(tempoObjetivo) {
     segundos +
     " segundos"
   );
+} else {
+  return "Prazo Finalizado";
 }
+}
+
+function atualizaCronometro() {
+  for (let i = 0; i < contadores.length; i++) {
+    contadores[i].textContent = calculaTempo(tempos[i]);
+  }
+}
+
+function comecaCronometro() {
+  atualizaCronometro();
+  setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
